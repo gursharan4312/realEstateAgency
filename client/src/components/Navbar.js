99a8dbe1cd541eb2030d32f36417161209098327
Gursharan Singh <gursharansingh4312@gmail.com>
2021-01-21 15:30:31 -0800

    navbar created

--- client/src/components/Navbar.js
+++ client/src/components/Navbar.js
@@ -1,8 +1,65 @@
+import styled, { css } from "styled-components";
+import { Link } from "react-router-dom";
+import { menuData } from "../data/MenuData";
+import { Button } from "./Button";
+
+const Nav = styled.nav`
+  height: 60px;
+  display: flex;
+  justify-content: space-between;
+  padding: 1rem 2rem;
+  z-index: 100;
+  position: fixed;
+  width: 100%;
+  background: red;
+`;
+
+const NavLink = css`
+  color: #fff;
+  display: inline-block;
+  align-items: center;
+  padding: 0 1rem;
+  height: 100%;
+  cursor: pointer;
+  text-decoration: none;
+`;
+
+const Logo = styled(Link)`
+  ${NavLink}
+  font-style: italic;
+`;
+const MenuBars = styled.i``;
+const NavMenu = styled.div`
+  display: flex;
+  align-items: center;
+`;
+
+const NavMenuLinks = styled(Link)`
+  ${NavLink}
+`;
+
+const NavBtn = styled.div`
+  display: flex;
+  align-items: center;
+  margin-right: 24px;
+`;
+
 function Navbar() {
   return (
-    <div>
-      <h1>Navbar</h1>
-    </div>
+    <Nav>
+      <Logo>REST</Logo>
+      <MenuBars />
+      <NavMenu>
+        {menuData.map((item, index) => (
+          <NavMenuLinks to={item.link} key={index}>
+            {item.title}
+          </NavMenuLinks>
+        ))}
+      </NavMenu>
+      <NavBtn>
+        <Button to="/contact">Contact Us</Button>
+      </NavBtn>
+    </Nav>
   );
 }
 
