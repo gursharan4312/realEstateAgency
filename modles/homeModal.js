2423a7999fea06e1cf9f55726acb2ae668d41c41
Gursharan Singh <gursharansingh4312@gmail.com>
2021-02-10 16:51:11 -0800

    home modal created

---
+++ modles/homeModal.js
@@ -0,0 +1,26 @@
+import mongoose from "mongoose";
+
+const homeSchema = mongoose.Schema(
+  {
+    title: { type: String, required: true },
+    address: { type: String, required: true },
+    images: [String],
+    price: { type: Number, required: true },
+    type: { type: String, required: true },
+    date: Date,
+    rooms: { type: Number, required: true },
+    washrooms: { type: Number, required: true },
+    pets: Boolean,
+    size: { type: Number, required: true },
+    position: {
+      lat: Number,
+      lng: Number,
+    },
+    details: String,
+  },
+  {
+    timestamps: true,
+  }
+);
+
+const Home = mongoose.model("Home", homeSchema);
