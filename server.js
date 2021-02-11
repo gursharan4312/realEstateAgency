b6ccd032b0e0bb244afcdc5ee691103ca31df10f
Gursharan Singh <gursharansingh4312@gmail.com>
2021-02-10 17:08:29 -0800

    database connected

--- server.js
+++ server.js
@@ -1,10 +1,14 @@
 import path from "path";
 import express from "express";
 import morgan from "morgan";
+import dotenv from "dotenv";
 import colors from "colors";
+import connectDb from "./config/db.js";
 import { errorHandler, notFound } from "./middleware/errorMiddleware.js";
 import homesRoutes from "./routes/homeRoutes.js";
 const app = express();
+dotenv.config();
+connectDb();
 
 app.use(express.json());
 app.use(morgan("dev"));
