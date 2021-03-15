3a21ab6b44836ac740888716cb5231981b489f15
Gursharan Singh <gursharansingh4312@gmail.com>
2021-03-14 17:54:44 -0700

    production server added

--- server.js
+++ server.js
@@ -17,6 +17,18 @@ app.use(morgan("dev"));
 const __dirname = path.resolve();
 app.use("/uploads", express.static(path.join(__dirname, "/uploads")));
 
+if (process.env.NODE_ENV === "production") {
+  app.use(express.static(path.resolve(__dirname, "frontend", "build")));
+
+  app.get("/*", (req, res) =>
+    res.sendFile(path.resolve(__dirname, "frontend", "build", "index.html"))
+  );
+} else {
+  app.get("/", (req, res) => {
+    res.send("API is running");
+  });
+}
+
 app.get("/", (req, res) => {
   res.send("API is running");
 });
